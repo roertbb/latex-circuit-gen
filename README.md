94a7ce617ba502761dbb546bd3a8b46dbb9ceadb
roertbb <robert.banaszak1997@gmail.com>
2017-07-31 09:44:53 +0200

    basic template

---
+++ README.md
@@ -0,0 +1,5 @@
+# latex-circuit-gen
+
+During studying at University of Technology in Poznań we were ment to create reports for subject called Basic of Electronics in latex. Circuits were part of our assignment. Creating such a script saved us lot of time and was pretty fun :D
+
+# more information will be here soon
--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 let canvas = document.querySelector("canvas"),
 	ctx = canvas.getContext("2d"),
-	width = canvas.width = 640,
-	height = canvas.height = 480;
+	width = canvas.width = 660,
+	height = canvas.height = 480,
+	tile = 30;
+
+function drawResistor(x, y direction) {
+	ctx.strokeStyle = "#000";
+	ctx.beginPath();
+	ctx.moveTo(x,y-tile);
+	ctx.lineTo(x+tile/2,y-tile);
+	ctx.lineTo(x+tile/2,y-tile/2);
+	ctx.lineTo(x+3/2*tile,y-tile*3/2);
+	ctx.lineTo(x+tile/2,y-tile*3/2);
+	ctx.lineTo(x+tile/2,y-tile);
+	ctx.moveTo(x+tile*3/2,y+tile);
+	ctx.moveTo(x+tile*2,y+tile);
+	ctx.stroke();
+
+}
+
+function draw() {
+	ctx.clearRect(0,0,width,height);
+	ctx.fillStyle = "#f7f7f7";
+	ctx.fillRect(0,0,width,height);
+
+	//draw grid
+	ctx.strokeStyle = "#ddd";
+	ctx.beginPath();
+	for (let i=tile; i<height; i+=tile) {
+		ctx.moveTo(tile,i);
+		ctx.lineTo(width-tile,i);
+	}
+	for (let i=tile; i<width; i+=tile) {
+		ctx.moveTo(i,tile);
+		ctx.lineTo(i,height-tile);
+	}
+	ctx.stroke();
+
+	drawResistor(tile,tile);
+}
 
 function update() {
 
-	ctx.clearRect(0,0,width,height);
-	ctx.fillStyle = "#ddd";
-	ctx.fillRect(0,0,width,height);
+	draw();
 
 	requestAnimationFrame(update);
 }
